7c5e5fdc9ccca3c9d04fd8354e87a7d8760bdf2b
John Kilonzi <mrjohnkilonzi@gmail.com>
2019-10-01 10:23:38 +0300

    Resolved issues from Validator, including adding some headings and changing some sections to div

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     "extends": "stylelint-config-recommended",
     "rules": {
-        "at-rule-empty-line-before": ["never", {
+        "at-rule-empty-line-before": ["always", {
             except: [
                 "blockless-after-same-name-blockless",
                 "first-nested",
@@ -81,7 +81,7 @@ module.exports = {
         "number-leading-zero": "always",
         "number-no-trailing-zeros": true,
         "property-case": "lower",
-        "rule-empty-line-before": ["never", {
+        "rule-empty-line-before": ["always-multi-line", {
             except: ["first-nested"],
             ignore: ["after-comment"],
         }],
